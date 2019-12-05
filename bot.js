61b1e3b887d8675a34c71666d75fa825c6d0449b
Silvershot335 <silvershot335@gmail.com>
2019-12-05 14:04:29 -0600

    Lots of stuff

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.io");
 const SilverBot = new Discord.Client({
-  token: "NjUyMTg1MzU1MjgxMjM1OTY4.XelHiA.pnsX64SUh-Ef85U1wvs5VYVlV-c",
+  token: "NjUyMTg1MzU1MjgxMjM1OTY4.XelLHw.r-poR31w7D1RiBaPKpPjzlCfDgE",
   autorun: true
 });
 
@@ -27,31 +27,55 @@ SilverBot.on("disconnect", (errMsg, code) => {
 });
 
 SilverBot.on("message", (user, userID, channelID, message, event) => {
-    if (message === "ping") {
-        SilverBot.sendMessage({
-            to: channelID,
-            message: "pong"
-        });
-        console.log(channelID)
-    }
-})
+  if (message === "ping") {
+    SilverBot.sendMessage({
+      to: channelID,
+      message: "pong"
+    });
+    console.log(channelID);
+  }
+  if (message === "magcord") {
+    SilverBot.sendMessage({
+      to: channelID,
+      message: "https://discord.gg/TAsqJer"
+    });
+    console.log(channelID);
+  }
+ /* if (message === "Who is Magdalena Bay?") {
+    SilverBot.sendMessage({
+      to: channelID,
+      message:
+        "Magdalena Bay is a pop duo comprised of Mica Tenenbaum (songwriting and vocals) and Matthew Lewin (songwriting, vocals and production). The duo has been writing together since high school and started making pop as Magdalena Bay in 2016. Inspiration is drawn from retro pop songwriting and contemporary production-- the result has been a collection of upbeat and synth-driven singles."
+    });
+    console.log(channelID);
+  }
+  /*if (message === "lying whore") {
+    SilverBot.sendMessage({
+      to: channelID,
+      message: "judy"
+    });
+    console.log(channelID);
+  }*/
+});
+
 
 SilverBot.on("message", (user, userID, channelID, message, event) => {
-    if (message === "Who is Magdalena Bay?") {
-        SilverBot.sendMessage({
-            to: channelID,
-            message: "Magdalena Bay is a pop duo comprised of Mica Tenenbaum (songwriting and vocals) and Matthew Lewin (songwriting, vocals and production). The duo has been writing together since high school and started making pop as Magdalena Bay in 2016. Inspiration is drawn from retro pop songwriting and contemporary production-- the result has been a collection of upbeat and synth-driven singles."
-        });
-        console.log(channelID)
-    }
-})
-
-SilverBot.on("message", (user, userID, channelID, message, event) => {
-    if (message === "poecs") {
-        SilverBot.sendMessage({
-            to: channelID,
-            message: "https://silvershot335.github.io/PoECheatSheet/"
-        });
-        console.log(channelID)
-    }
-})
+  if (message.includes("poecs") || message.includes("poe cheat sheet")) {
+    SilverBot.sendMessage({
+      to: channelID,
+      message: `https://silvershot335.github.io/PoECheatSheet/`
+    });
+  }
+  if (message.includes("who is magdalena bay") || message.includes("who is magbay")) {
+      SilverBot.sendMessage({
+          to: channelID,
+          message: `Magdalena Bay is a pop duo comprised of Mica Tenenbaum (songwriting and vocals) and Matthew Lewin (songwriting, vocals and production). The duo has been writing together since high school and started making pop as Magdalena Bay in 2016. Inspiration is drawn from retro pop songwriting and contemporary production-- the result has been a collection of upbeat and synth-driven singles.`
+      })
+  }
+  if (message.includes("vu")||(message.includes("Vu"))) {
+      SilverBot.sendMessage({
+          to: channelID,
+          message: `Please refrain from bringing him up. Thank you.`
+      })
+  }
+});
