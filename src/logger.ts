42bbc23c1f2dcbb229edcc982a330eea283d0714
Silvershot335 <silvershot335@gmail.com>
2020-03-06 13:20:36 -0600

    roles added

--- src/logger.ts
+++ src/logger.ts
@@ -3,6 +3,8 @@ export const logger = winston.createLogger({
   level: 'debug',
   format: winston.format.colorize(),
   transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
     new winston.transports.Console({ format: winston.format.simple() })
   ]
 });
--- src/points.ts
+++ src/points.ts
@@ -56,15 +56,15 @@ export function giveUserPoints(message: Message) {
         (level) => previousPoints < level.experience
       );
       // If there is a level with more xp than they currently have
-      if (currentLevel) {
+      /*if (currentLevel) {
         // If the amount of points gained for
         // the current message + their current points > how many xp required to level up
-     /*   if (previousPoints + points > currentLevel.experience) {
+        if (previousPoints + points > currentLevel.experience) {
           message.channel.send(
             'Congratulations, you leveled up to level ' + currentLevel.level
           );
-        }*/
-      }
+        }
+      }*/
       Point.create({ userID, timestamp, points, serverID })
         .save()
         .catch(logger.error);
