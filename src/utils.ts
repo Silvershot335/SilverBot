bb8ed3419ea0cb504f6a74c7dca278cea158c6b1
Judah Goff <35714383+bgoff1@users.noreply.github.com>
2020-10-20 01:05:54 -0500

    refactor vu command (#16)

--- src/utils.ts
+++ src/utils.ts
@@ -77,6 +77,10 @@ export function getRandomNumber(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+export function getRandomItem<T>(array: T[]): T {
+  return array[getRandomNumber(0, array.length - 1)];
+}
+
 export function getNow() {
   const date = new Date();
   return (
